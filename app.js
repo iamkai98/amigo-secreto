242ef6a82b3dfaf33da5ec44a4b1f59ecc5d5cce
iamkai98 <kapconc2009@gmail.com>
2025-08-12 02:17:34 -0300

    second commit

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
+let listaDeAmigos = [];
 
 
 function adicionarAmigo(){
+let campoAmigo = document.querySelector('input').value;
 
-
+if (campoAmigo == ''){
+    alert("Por favor, insira um nome.")
+}
+else{
+    listaDeAmigos.push(campoAmigo);
+    atualizarListaDeAmigos();
 }
 
-function sortearAmigo(){
+console.log(listaDeAmigos);
+limparCampo();
+}
 
-    
+
+function atualizarListaDeAmigos(){
+    let lista = document.getElementById("listaAmigos");
+
+    lista.innerHTML = '';
+
+    for (let i = 0; i < listaDeAmigos.length; i++){
+        lista.innerHTML += `<li>${listaDeAmigos[i]}</li>`;
+    }
+}
+
+
+
+
+function limparCampo(){
+    campoAmigo = document.querySelector('input');
+    campoAmigo.value = ''
 }
